29aa1b45f37af8d95dddfee3391d49fac9fbcf36
Eugenio <eugeniolimaeugenio5000@outlook.com>
2023-04-22 19:24:59 -0300

    update seeder

--- Infrastructure/Seeder.cs
+++ Infrastructure/Seeder.cs
@@ -17,23 +17,129 @@ namespace Infrastructure
                     {
                         Nick = "Eugenio Lopes",
                         Email = "eugenio-lopes@batteries.com",
+                        RememberMe = true,
                         CreatedAt = DateTimeOffset.Now,
                         Password = "Passwd123456#",
-                        ExpiryTime = DateTime.Now.AddDays(1),
+                        ExpiryTime = DateTime.Now.AddDays(30),
                         RefreshToken = Guid.NewGuid()
 
                     },
                     new()
                     {
                         Nick = "Daniel Correia",
-                        Email = "Daniel-Correia@batteries.com",
+                        Email = "daniel-correia@batteries.com",
+                        RememberMe = true,
                         CreatedAt = DateTime.Now,
                         Password = "Passwd123456#",
-                        ExpiryTime = DateTime.Now.AddDays(1),
+                        ExpiryTime = DateTime.Now.AddDays(30),
                         RefreshToken = Guid.NewGuid()
+                    },
+                    new()
+                    {
+                        Nick = "Carlos Gama",
+                        Email = "carlos-gama@batteries.com",
+                        RememberMe = true,
+                        CreatedAt = DateTimeOffset.Now,
+                        Password = "Passwd123456#",
+                        ExpiryTime = DateTime.Now.AddDays(30),
+                        RefreshToken = Guid.NewGuid()
+
                     },
                 };
                 context.Users.AddRange(users);
+                context.SaveChanges();
+            };
+            
+            if (!context.Companies.Any())
+            {
+                var companies = new List<Company>
+                {
+                    new()
+                    {
+                        Title = "Company 01",
+                        Address = "Rua Dom Felício Vasconcelos - Capiata",
+                        CreatedAt = DateTimeOffset.Now,
+                    },
+                    new()
+                    {
+                        Title = "Company 02",
+                        Address = "Ponta Verde",
+                        CreatedAt = DateTimeOffset.Now,
+                    },
+                    new()
+                    {
+                        Title = "Company 03",
+                        Address = "Rua Pedro Florentim Bastos - Polém",
+                        CreatedAt = DateTimeOffset.Now,
+                    },
+                };
+                context.Companies.AddRange(companies);
+                context.SaveChanges();
+            };
+
+            if (!context.CompanyBenefits.Any())
+            {
+                var companyBenefits = new List<CompanyBenefit>
+                {
+                    new()
+                    {
+                        Benefit = "Plano de saúde, Vale alimentação, Vale bem-estar, Vale Ifood",
+                        Description = "Descrição do benefício 01",
+                        Disabled = false,
+                        CompanyId = 1,
+                        CreatedAt = DateTimeOffset.Now,
+                        ScoreNeeded = 60,
+                    },
+                    new()
+                    {
+                        Benefit = "Plano de saúde, Vale alimentação",
+                        Description = "Descrição do benefício 02",
+                        Disabled = false,
+                        CompanyId = 2,
+                        CreatedAt = DateTimeOffset.Now,
+                        ScoreNeeded = 70,
+                    },
+                    new()
+                    {
+                        Benefit = "Plano de saúde, Vale alimentação, Vale bem-estar",
+                        Description = "Descrição do benefício 03",
+                        Disabled = false,
+                        CompanyId = 3,
+                        CreatedAt = DateTimeOffset.Now,
+                        ScoreNeeded = 50,
+                    },
+                };
+                context.CompanyBenefits.AddRange(companyBenefits);
+                context.SaveChanges();
+            }
+            
+            if (!context.UserCompanyScores.Any())
+            {
+                var userCompanyScores = new List<UserCompanyScores>
+                {
+                    new()
+                    {
+                        Scores = 100,
+                        CompanyId = 1,
+                        UserId = 1,
+            
+                    },
+                    new()
+                    {
+                        Scores = 80,
+                        CompanyId = 2,
+                        UserId = 2,
+            
+                    },
+                    new()
+                    {
+                        Scores = 60,
+                        CompanyId = 3,
+                        UserId = 2,
+            
+                    },
+                };
+                context.UserCompanyScores.AddRange(userCompanyScores);
                 context.SaveChanges();
             }
         }
