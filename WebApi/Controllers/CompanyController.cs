4c47e152db8f64314e6ba0b51b3cba1f1aea712a
Eugenio <eugeniolimaeugenio5000@outlook.com>
2023-05-01 02:41:30 -0300

    hotfix/#Adicionando verificacao para saber se usuario existe no banco

---
+++ WebApi/Controllers/CompanyController.cs
@@ -0,0 +1,277 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Models.Params;
+using Infrastructure;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class CompanyController: ControllerBase
+    {
+
+        private readonly BatteriesProjectDbContext _dbContext;
+        
+        public CompanyController(BatteriesProjectDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// Listar os dados de perfil da empresa
+        /// </summary>
+        /// <param name="id">O id da empresa</param>
+        /// <param name="cancellationToken">Usado para cancelar a requisição</param>
+        /// <returns>Ok({id, titulo, endereco})</returns>
+        [AllowAnonymous]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            if (!await _dbContext.Companies
+                        .AnyAsync(c => c.Id == id, cancellationToken))
+            {
+                return NotFound("Unable to find company");
+            }
+
+            var companyProfileData = await _dbContext.Companies
+                .AsNoTracking()
+                .Where(c => c.Id == id)
+                .Select(c => new
+                {
+                    Id = c.Id,
+                    Title = c.Title,
+                    Address = c.Address
+                })
+                .ToListAsync(cancellationToken);
+
+            return Ok(companyProfileData);
+        }
+        
+        /// <summary>
+        /// Cadastrar empresa
+        /// </summary>
+        /// <param name="company">a empresa a ser cadastrada</param>
+        /// <param name="cancellationToken">Usado para cancelar a requisição</param>
+        /// <returns>Status Code 201 (Created)</returns>
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] CompanyModel company, CancellationToken cancellationToken)
+        {
+            
+            if (await _dbContext.Companies
+                    .AnyAsync(c => c.Title == company.Title, cancellationToken))
+            {
+                return BadRequest("Company already exists");
+            }
+
+            var newCompany = new Company()
+            {
+                Title = company.Title,
+                Address = company.Address,
+                CreatedAt = DateTimeOffset.Now,
+                Benefits = new List<CompanyBenefit>(),
+                Users = new List<UserCompanyScores>()
+            }; 
+
+            await _dbContext.Companies.AddAsync(newCompany, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Created(nameof(GetByIdAsync), newCompany);
+
+        }
+
+        /// <summary>
+        /// Deletar empresa
+        /// </summary>
+        /// <param name="id">O id da empresa</param>
+        /// <param name="cancellationToken">Usado para cancelar a requisição</param>
+        /// <returns>NoContent()</returns>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCompanyByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            if (!await _dbContext.Companies.AnyAsync(c => c.Id == id, cancellationToken))
+            {
+                return NotFound("Unable to find company");
+            }
+
+            var dbCompany = await _dbContext.Companies
+                .AsNoTracking()
+                .SingleOrDefaultAsync(c => c.Id == id, cancellationToken);
+            
+            _dbContext.Companies.Remove(dbCompany);
+            _dbContext.SaveChangesAsync(cancellationToken);
+
+            return NoContent();
+        }
+        
+        /// <summary>
+        /// Desabilita o beneficio de uma empresa
+        /// </summary>
+        /// <param name="id">O id da empresa</param>
+        /// <param name="cancellationToken">Usado para cancelar a requisição</param>
+        /// <returns>Ok()</returns>
+        [HttpDelete("{id}/benefits")]
+        public async Task<IActionResult> DeleteByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            //verificando se a empresa, em questão, contém benefícios
+            var benefits = await _dbContext.Companies
+                        .Where(c => c.Id == id)
+                        .Include(c => c.Benefits)
+                        .SelectMany(cb => cb.Benefits)
+                        .ToListAsync(cancellationToken);
+
+            if (benefits.Count == 0)
+            {
+                return NotFound("This company has no benefits");
+            }
+
+            var companyBenefits = await _dbContext.Companies
+                .Where(c => c.Id == id)
+                .Include(c => c.Benefits)
+                .SelectMany(c => c.Benefits)
+                .ToListAsync(cancellationToken);
+
+                foreach (var companyBenefit in companyBenefits)
+                {
+                    companyBenefit.Disabled = false;
+                }
+                
+            _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// Atualizar as configurações da empresa
+        /// </summary>
+        /// <param name="companyId">O id da empresa</param>
+        /// <param name="benefitId">O id do beneficio</param>
+        /// <param name="companyBenefit">o objeto companyBenefits contendo os novos beneficios</param>
+        /// <param name="cancellationToken">Usado para cancelar a requisição</param>
+        /// <returns>Created()</returns>
+        [HttpPut("{companyId}/benefit/{benefitId}")]
+        public async Task<IActionResult> PutAsync(int companyId, int benefitId, [FromBody] CompanyBenefitsModel companyBenefit, CancellationToken cancellationToken)
+        {
+            if (!await _dbContext.Companies
+                    .AnyAsync(c => c.Id == companyId, cancellationToken))
+            {
+                return NotFound("Unable to find company");
+            }
+
+            if (await _dbContext.Companies
+                    .Where(c => c.Id == companyId)
+                    .SelectMany(c => c.Benefits)
+                    .AnyAsync(cb => cb.Id == benefitId, cancellationToken))
+            {
+                return NotFound($"Unable to find benefit");
+            }
+            
+            var dbBenefit = await _dbContext.Companies
+                .Where(c => c.Id == companyId)
+                .SelectMany(c => c.Benefits)
+                .SingleOrDefaultAsync(cb => cb.Id == benefitId,cancellationToken);
+
+            //atualizando a referencia do banco
+            dbBenefit.Benefit = companyBenefit.Benefit;
+            dbBenefit.Description = companyBenefit.Description;
+            dbBenefit.ScoreNeeded = companyBenefit.ScoreNeeded;
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Created(nameof(GetByIdAsync), dbBenefit);
+
+        }
+
+        /// <summary>
+        /// Adicionar pontos ao usuário // cadastrar um novo UserCompanyScore
+        /// </summary>
+        /// <param name="id">o id da empresa</param>
+        /// <param name="userCompanyScoresModel">O novo UserCompanyScore a ser inserido no banco</param>
+        /// <param name="cancellationToken">Usado para cancelar a requisição</param>
+        /// <returns>Created()</returns>
+        [HttpPost("{id}/user")]
+        public async Task<IActionResult> PostUserCompanyScoresAsync(int id, [FromBody] UserCompanyScoresModel userCompanyScoresModel, CancellationToken cancellationToken)
+        {
+            if (id != userCompanyScoresModel.CompanyId)
+            {
+                return BadRequest("The CompanyId of the url is different from the CompanyId of the body");
+            }
+            
+            if (!await _dbContext.Companies
+                    .AnyAsync(c => c.Id == id, cancellationToken))
+            {
+                return NotFound("Unable to find company");
+            }
+            
+            if (!await _dbContext.Users
+                    .AnyAsync(u => u.Id == userCompanyScoresModel.userId, cancellationToken))
+            {
+                return NotFound("Unable to find user");
+            }
+
+            //verificando se o objeto a ser inserido já existe no banco | erro de duplicata
+            if (await _dbContext.UserCompanyScores
+                    .AnyAsync(ucs =>
+                        ucs.CompanyId == id && ucs.UserId == userCompanyScoresModel.userId, cancellationToken))
+            {
+                return BadRequest($"CompanyId and UserId must be an unique value. " +
+                                  $"The record with UserId = {userCompanyScoresModel.userId} and CompanyId = {userCompanyScoresModel.CompanyId} already exists");
+            }
+            
+            //instanciando um novo objeto de UserCompanyScores com base nos valores de userCompanyScoresModel 
+            var newUsc = new UserCompanyScores()
+            {
+                Scores = userCompanyScoresModel.Scores,
+                CompanyId = userCompanyScoresModel.CompanyId,
+                UserId = userCompanyScoresModel.userId
+            };
+
+            await _dbContext.UserCompanyScores.AddAsync(newUsc, cancellationToken);
+            
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Created(nameof(GetByIdAsync),newUsc);
+        }
+        
+        /// <summary>
+        /// Atualiza as configurações da empresa
+        /// </summary>
+        /// <param name="id">O id da company a ser atualizada</param>
+        /// <param name="companyModel">Os novos dados da company</param>
+        /// <param name="cancellationToken">Usado para cancelar a requisição</param>
+        /// <returns>Ok()</returns>
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> PatchAsync(int id, [FromBody] CompanyModel companyModel ,CancellationToken cancellationToken)
+        {
+            //verificando se uma determinado company existe no banco
+            if (!await _dbContext.Companies
+                    .AnyAsync(c => c.Id == id, cancellationToken))
+            {
+                return NotFound("Unable to find Company");
+            }
+            
+            //verificando se existe alguma company, no banco, cujo title é igual ao title do objeto vindo como parametro 
+            if (await _dbContext.Companies
+                    .AnyAsync(c => c.Title == companyModel.Title, cancellationToken))
+            {
+                return NotFound($"Company title named '{companyModel.Title}' already exists, please insert another name for title");
+            }
+
+            var dbCompany = await _dbContext.Companies
+                    .SingleOrDefaultAsync(c => c.Id == id, cancellationToken);
+
+            dbCompany.Title = companyModel.Title;
+            dbCompany.Address = companyModel.Address;
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return Ok();
+
+        }
+    }
+}
