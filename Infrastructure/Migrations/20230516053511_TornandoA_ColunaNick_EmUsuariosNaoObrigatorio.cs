b9ee66ffe982a8f1f45ca8bbdaf53efec0085b86
Eugenio <eugeniolimaeugenio5000@outlook.com>
2023-05-16 20:48:31 -0300

    Definindo a coluna Nick como não obrigatoria

---
+++ Infrastructure/Migrations/20230516053511_TornandoA_ColunaNick_EmUsuariosNaoObrigatorio.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class TornandoA_ColunaNick_EmUsuariosNaoObrigatorio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nick",
+                table: "Users",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nick",
+                table: "Users",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+    }
+}
