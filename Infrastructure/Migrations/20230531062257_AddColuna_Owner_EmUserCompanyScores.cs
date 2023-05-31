27e77fa3c7e589a2c55e5ebcbf8f801e5b2906e5
Eugenio Lopes Fernandes Lima <eugeniolimaeugenio5000@outlook.com>
2023-05-31 03:28:17 -0300

    Adicionando migration

---
+++ Infrastructure/Migrations/20230531062257_AddColuna_Owner_EmUserCompanyScores.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddColuna_Owner_EmUserCompanyScores : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Owner",
+                table: "UserCompanyScores",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Owner",
+                table: "UserCompanyScores");
+        }
+    }
+}
