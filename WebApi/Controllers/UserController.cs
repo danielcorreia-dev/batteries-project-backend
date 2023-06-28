42f52f903ebf4b08bc75a23145e891bc0e6e1cab
Daniel Correia <danielgalego.dev@gmail.com>
2023-06-28 17:57:11 -0300

    Revert "Merge branch 'hotfix/0.0.1'"
    This reverts commit b3f5d6ab6b522e1214e5d5e339dc1986aa8a8f0d.

--- WebApi/Controllers/UserController.cs
+++ WebApi/Controllers/UserController.cs
@@ -238,7 +238,7 @@ namespace WebApi.Controllers
                 return NotFound("Unable to find User");
             }
 
-            var userCompany = await _dbContext.Companies
+            var userCompany = await _dbContext.UserCompanyScores
                 .AsNoTracking()
                 .Where(ucs => ucs.UserId == id)
                 .Select(ucs => new
