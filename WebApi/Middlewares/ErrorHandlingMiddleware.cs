4445bf87fe8e85db249980b09dacc38db9c66185
Eugenio Lopes Fernandes Lima <eugeniolopesfernandeslima1997@outlook.com>
2023-07-01 03:10:18 -0300

    Adicionando middleware de exceção
    Obs: O Status Code a ser retornado, sempre, no body da resposta http será o 500 (Server Internal Error).

---
+++ WebApi/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Models.Results;
+using Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebApi.Middlewares
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private async Task<IActionResult> HandleExceptionAsync(HttpContext httpContext, Exception ex)
+        {
+
+            var dbContext =
+                (BatteriesProjectDbContext)httpContext.RequestServices.GetService(typeof(BatteriesProjectDbContext));
+
+            var errorLog = new ErrorLog()
+            {
+                Message = ex.Message,
+                Source = ex.TargetSite?.ToString(),
+                Timestamp = DateTimeOffset.Now.ToString(),
+                Type = ex.GetType().FullName,
+                StackTrace = ex.StackTrace,
+                TraceId = Guid.NewGuid(),
+            };
+
+            await dbContext!.ErrorLogs.AddAsync(errorLog);
+            await dbContext.SaveChangesAsync();
+            
+            var responseObj = new ErrorLogModel
+            {
+                StatusCode = 500,
+                ContentType = "application/json",
+                TraceId = errorLog.TraceId,
+                Type = errorLog.Type,
+            };
+
+            await httpContext.Response.WriteAsJsonAsync(responseObj);
+            
+            return new EmptyResult();
+        }
+    }
+}
